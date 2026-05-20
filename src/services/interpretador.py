7edced13811fa31456f609e3ca049de9fdebc7b1
Diegolinop <diegopimentasuarez@gmail.com>
2026-05-20 18:29:31 -0300

    feat(interpretador): adicionado método para salvar os indices na memoria

--- src/services/interpretador.py
+++ src/services/interpretador.py
@@ -17,6 +17,17 @@ class Interpretador:
         self.indice_genero = self.__carregar_indice_secundario(CAMINHO_INDICE_GENEROS)
         self.indice_publicadora = self.__carregar_indice_secundario(CAMINHO_INDICE_PUBLICADORAS)
 
+    def salvar_todos_os_indices(self):
+        self.__salvar_indice(CAMINHO_INDICE_PRIMARIO, self.indice_primario)
+        self.__salvar_indice(CAMINHO_INDICE_GENEROS, self.indice_genero)
+        self.__salvar_indice(CAMINHO_INDICE_PUBLICADORAS, self.indice_publicadora)
+        self.__salvar_indice(CAMINHO_LISTA_INVERTIDA, self.lista_invertida)
+
+    def __salvar_indice(self, caminho, dados):
+        with open(caminho, 'w', encoding='utf-8') as arquivo:
+            for entrada in dados:
+                arquivo.write(f"{entrada[0]}|{entrada[1]}\n")
+
     def __carregar_lista_invertida(self):
         if not os.path.exists(CAMINHO_LISTA_INVERTIDA):
             print("Erro: Arquivo de lista invertida não encontrado.")
