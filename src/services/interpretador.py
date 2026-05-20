7d90c491c3163fb0dae55818d12abe462c7ea9b8
Diegolinop <diegopimentasuarez@gmail.com>
2026-05-20 09:10:55 -0300

    feat: adicionado interpretador de operações simples

--- src/services/interpretador.py
+++ src/services/interpretador.py
@@ -2,20 +2,20 @@ from constantes import *
 import os
 
 class Interpretador:
-    def __init__(self, caminho_operacoes: str = "arquivo_operacoes") -> None:
+    def __init__(self, caminho_operacoes: str = "arquivo_operacoes"):
         self.caminho_operacoes = caminho_operacoes
         
         self.indice_genero = []
         self.indice_publicadora = []
         self.lista_invertida = []
 
-    def carregar_todos_os_indices(self) -> None:
+    def carregar_todos_os_indices(self):
         self.__carregar_lista_invertida()
         
         self.indice_genero = self.__carregar_indice_secundario(CAMINHO_INDICE_GENEROS)
         self.indice_publicadora = self.__carregar_indice_secundario(CAMINHO_INDICE_PUBLICADORAS)
 
-    def __carregar_lista_invertida(self) -> None:
+    def __carregar_lista_invertida(self):
         if not os.path.exists(CAMINHO_LISTA_INVERTIDA):
             print("Erro: Arquivo de lista invertida não encontrado.")
             return
@@ -29,7 +29,7 @@ class Interpretador:
                 id_jogo, proximo_ponteiro = linha.split("|")
                 self.lista_invertida.append([id_jogo, int(proximo_ponteiro)])
 
-    def __carregar_indice_secundario(self, caminho_indice: str) -> list:
+    def __carregar_indice_secundario(self, caminho_indice: str):
             if not os.path.exists(caminho_indice):
                 return []
 
@@ -45,7 +45,7 @@ class Interpretador:
             indice_temporario.sort() 
             return indice_temporario
 
-    def __ler_operacoes(self) -> str:
+    def __ler_operacoes(self):
         if not os.path.exists(self.caminho_operacoes):
             raise FileNotFoundError(f"Não foi encontrado arquivo {self.caminho_operacoes}.")
         
@@ -56,3 +56,13 @@ class Interpretador:
         lst_operacoes = str_operacoes.split("\n")
 
         return lst_operacoes
+
+    def interpretar_operacoes(self):
+        # usa tipo ids pra identificar as operações pra não precisar iterar pelas strings toda vez q rodar uma operação (0=bp,1=bs1,etc.)
+        # TODO: revisar se esse é o melhor jeito de fazer isso
+        lst_operacoes = __ler_operacoes()
+        for i in lst_operacoes:
+            for op in range(len(LISTA_OPERACOES)):
+                if i == LISTA_OPERACOES[op]:
+                    return op
+        return -1
